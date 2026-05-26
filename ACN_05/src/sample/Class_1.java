11dc7e659723cfb1d55830c010dce1d9cf042fde
sruthi <vangapallivinith@gmail.com>
2026-05-26 15:46:18 +0530

    Day2 Java programs

---
+++ ACN_05/src/sample/Class_1.java
@@ -0,0 +1,22 @@
+//package sample;
+//
+//public class Class_1 {
+//
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//
+//	}
+//
+//}
+
+
+package sample;
+public class Class_1{
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Agent-AI");
+	}
+	
+	
+}
