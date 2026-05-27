02da9e4a2bad98c8d7f4f166d46d054c6cbfa6bc
sruthi <vangapallivinith@gmail.com>
2026-05-27 16:15:03 +0530

    Day - 4 -- Packages(pack2)

---
+++ ACN_05/src/com/tnsif/packs/pack2/c3.java
@@ -0,0 +1,19 @@
+package com.tnsif.packs.pack2;
+
+import com.tnsif.packs.pack1.c1;
+
+public class c3 extends c1{
+	public void test2()
+	{
+		c3 ob2 = new c3();
+		//System.out.println(ob.a); due to private AM
+		//System.out.println(ob.b);
+		System.out.println(ob2.c);
+		System.out.println(ob2.d);
+		
+		//ob.show1();
+		//ob.show2();
+		ob2.show3();
+		ob2.show4();
+	}
+}
---
+++ ACN_05/src/com/tnsif/packs/pack2/c4.java
@@ -0,0 +1,19 @@
+package com.tnsif.packs.pack2;
+
+import com.tnsif.packs.pack1.c1;
+
+public class c4 {
+	public void test3()
+	{
+		c1 ob = new c1();
+		//System.out.println(ob.a); due to private AM
+		//System.out.println(ob.b);
+		//System.out.println(ob.c);
+		System.out.println(ob.d);
+		
+		//ob.show1();
+		//ob.show2();
+		//ob.show3();
+		ob.show4();
+	}
+}
